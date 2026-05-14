0a2904d777e0fd9e8ea098860a8844ff41d2d97a
Iamfemimorin <martmorin1@gmail.com>
2026-05-14 11:42:13 +0100

    added files to the code

---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    // Member variables
+    public string _name = "";
+
+    public List<Job> _jobs = new List<Job>();
+
+    // Method to display resume details
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Loop through each job and display it
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
