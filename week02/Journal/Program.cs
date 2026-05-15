c16dfc48caddacb78df6ed7d9813da588804a5e9
Iamfemimorin <martmorin1@gmail.com>
2026-05-15 19:33:16 +0100

    added modification to show abstraction development

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string input = Console.ReadLine();
+            choice = int.Parse(input);
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+
+                string response = Console.ReadLine();
+
+                DateTime currentTime = DateTime.Now;
+                string dateText = currentTime.ToShortDateString();
+
+                Entry newEntry = new Entry();
+
+                newEntry._date = dateText;
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == 2)
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+
+                theJournal.LoadFromFile(filename);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+
+                theJournal.SaveToFile(filename);
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+        }
     }
 }
